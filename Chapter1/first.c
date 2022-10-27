3b8e3665d7d30f022dd9a4e0edb6c9fd875f3536
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-27 18:20:08 +0000

    First Program

--- Chapter1/first.c
+++ Chapter1/first.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+
+// ;--> Instruction Terminator
+// Execution of Programm is start with main function
 int main(){
-    printf("hello I am Muheet");
+    int integer_variable = 9;
+    float float_variable = 9.78;
+    char character = 'd';
+    printf("hello I am Muheet\n");
+    printf("integer_variable = %d float_variable = %f character = %c",integer_variable,float_variable,character);
+    // %f for floating number printing
     return 0;
 }
