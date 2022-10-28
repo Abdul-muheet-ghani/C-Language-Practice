b31659f74d1fc4fa27f7ff93544d5a05a0381a18
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-28 19:28:05 +0000

    chapter 2 practice 01

---
+++ Chapter2/instructions_1.c
@@ -0,0 +1,19 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    // variables 
+    int a = 4;         //type declaration instructions
+    printf();
+    return 0;
+}
