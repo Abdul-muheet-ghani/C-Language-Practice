fcd475c470a39a26de3f2c9e14da2a2fab349555
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-28 19:27:53 +0000

    practice 03

---
+++ Chapter1/triangle_practice.c
@@ -0,0 +1,21 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)"
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+#include <stdio.h>
+int main(){
+    // variables 
+    int length,depth;
+    
+    printf("Enter the length = ");
+    scanf ("%d" , &length);
+
+    printf("Enter the depth = ");
+    scanf ("%d" , &depth);
+
+    printf("Area = %d" , length * depth);
+    return 0;
+}
