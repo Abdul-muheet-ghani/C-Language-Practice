3c6ae6902adec23058e9a9bb88d5e59b6173381c
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-28 19:27:41 +0000

    Practice 01

--- Chapter1/first.c
+++ Chapter1/first.c
@@ -1,14 +1,24 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)"
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
 #include <stdio.h>
 
 
 // ;--> Instruction Terminator
 // Execution of Programm is start with main function
-int main(){
+int main(){    
     int integer_variable = 9;
     float float_variable = 9.78;
     char character = 'd';
     printf("hello I am Muheet\n");
     printf("integer_variable = %d float_variable = %f character = %c",integer_variable,float_variable,character);
-    // %f for floating number printing
+    // %f for real values number printing
     return 0;
 }
+
+//  int main means that it return value in integer 
+//  return 0; means int main return 0 which indicates the success for OS
