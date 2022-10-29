5581460c631781e84fd20aaea4d264194f5212a8
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-29 13:02:16 +0000

    updated

---
+++ Chapter2/divisibilty_02.c
@@ -0,0 +1,22 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+
+int main(){
+    // variables 
+    int num;
+    printf("Enter the number = ");
+    scanf("%d" , &num);
+    printf("\nIf answer = 0 then its divisible by 97 else not ANSWER = %d" , num%97);
+    return 0;
+}
