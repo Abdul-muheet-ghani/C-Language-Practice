f2c484adb09d8ef8996171b4fc7f441036aee2c9
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-30 07:46:40 +0000

    practice 01

---
+++ Chapter6/Array_basics01.c
@@ -0,0 +1,26 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    // variables 
+    int marks[5];
+    for(int i=0; i<5; i++){
+        printf("\nEnter the marks of student : %d : ", i+1);
+        scanf ("%d", &marks[i]);
+    }
+
+    for(int j=0; j<5; j++){
+        printf("\nThe marks of student : %d = %d", j+1, &marks[j]);
+    }
+    return 0;
+}
