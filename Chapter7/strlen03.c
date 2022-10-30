f4d806f21794b03fefef2423ef830cc70ac7702f
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-30 07:48:16 +0000

    practice 03

---
+++ Chapter7/strlen03.c
@@ -0,0 +1,22 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // variables 
+    char *str = "muheet";
+    int length = strlen(str);
+    printf("The length of the string = %d", length);
+    return 0;
+}
