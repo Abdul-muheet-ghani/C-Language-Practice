c987ad38f304752f8fdd0f56e4d446aaed67f627
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-10-30 07:48:04 +0000

    practice 02

---
+++ Chapter7/string02.c
@@ -0,0 +1,21 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    // variables 
+    char s[100];
+    printf("Enter your name: ");
+    scanf("%s", s);
+    printf("\nYour name is: %s", s);
+    return 0;
+}
