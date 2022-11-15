3cfd1e12c5c04b3139ffcb17818df53a8d3b3194
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-11-15 17:15:14 +0000

    output file

---
+++ Chapter10/file.c
@@ -0,0 +1,20 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    // variables 
+    FILE *ptr;
+    //ptr = fopen("sample_in","r");
+    ptr = fopen("sample_in","w");
+    return 0;
+}
